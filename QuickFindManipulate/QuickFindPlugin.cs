03279fb23817f4265f9b217c3dd10ee698a837c8
Natraj Yegnaraman <rajyraman@gmail.com>
2016-04-13 20:05:29 +1000

    Initial commit.

---
+++ QuickFindManipulate/QuickFindPlugin.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Linq;
+
+namespace RYR.QuickFindManipulate
+{
+    public class QuickFindPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                if (!context.InputParameters.Contains("Query")) return;
+
+                var query = (QueryExpression)context.InputParameters["Query"];
+                var likeValue = string.Empty;
+                var likeAttribute = string.Empty;
+                var primaryKeyFilter = new FilterExpression(LogicalOperator.Or);
+                var likeValueGuid = Guid.Empty;
+
+                foreach (var filter in query.Criteria.Filters.ToList())
+                {
+                    if (filter.IsQuickFindFilter)
+                    {
+                        foreach (var condition in filter.Conditions.ToList())
+                        {
+                            if (condition.Operator == ConditionOperator.Like)
+                            {
+                                likeValue = condition.Values?[0].ToString();
+                                likeAttribute = condition.AttributeName;
+                                if (!string.IsNullOrEmpty(likeValue) && likeValue.EndsWith("%"))
+                                {
+                                    likeValue = likeValue.Substring(0, likeValue.Length - 1);
+                                    var attributeDetail = RetrieveAttributeDetail(service, query.EntityName, condition.AttributeName);
+
+                                    if (Guid.TryParse(likeValue, out likeValueGuid))
+                                    {
+                                        primaryKeyFilter.AddCondition(attributeDetail.Item2, ConditionOperator.Equal, likeValue);
+                                        filter.Conditions.Remove(condition);
+                                    }
+                                    else
+                                    {
+                                        likeValue = string.Empty;
+                                    }
+                                }
+                                else
+                                {
+                                    likeValue = string.Empty;
+                                }
+                            }
+                        }
+                        if (filter.Conditions.Count == 0)
+                        {
+                            query.Criteria.Filters.Remove(filter);
+                        }
+                    }
+                }
+                if (likeValue != string.Empty)
+                {
+                    //HACK: This will not work for activities. Need to check from metadata.
+                    primaryKeyFilter.AddCondition($"{query.EntityName}id", ConditionOperator.Equal, likeValue);
+                    query.Criteria.AddFilter(primaryKeyFilter);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+
+        private Tuple<bool, string> RetrieveAttributeDetail(IOrganizationService service, string entityName, string attributeName)
+        {
+            var attributeMetadata = (RetrieveAttributeResponse)service.Execute(
+                new RetrieveAttributeRequest
+                {
+                    EntityLogicalName = entityName,
+                    RetrieveAsIfPublished = true,
+                    LogicalName = attributeName
+                });
+            if (!string.IsNullOrEmpty(attributeMetadata.AttributeMetadata.AttributeOf))
+            {
+                attributeMetadata = (RetrieveAttributeResponse)service.Execute(
+                    new RetrieveAttributeRequest
+                    {
+                        EntityLogicalName = entityName,
+                        RetrieveAsIfPublished = true,
+                        LogicalName = attributeMetadata.AttributeMetadata.AttributeOf
+                    });
+            }
+            var isValidForGuid = attributeMetadata.AttributeMetadata.AttributeType.Value == Microsoft.Xrm.Sdk.Metadata.AttributeTypeCode.Lookup ||
+                attributeMetadata.AttributeMetadata.AttributeType.Value == Microsoft.Xrm.Sdk.Metadata.AttributeTypeCode.Customer ||
+                attributeMetadata.AttributeMetadata.AttributeType.Value == Microsoft.Xrm.Sdk.Metadata.AttributeTypeCode.Uniqueidentifier;
+
+            return new Tuple<bool, string>(isValidForGuid, attributeMetadata.AttributeMetadata.LogicalName);
+        }
+    }
+}
